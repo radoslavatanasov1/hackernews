f1d31d9775bbe29839c4a1cdf2e43115fa660bf7
Radoslav Atanasov <radoslavatanasovoffical1@gmail.com>
2022-07-21 14:17:37 +0200

    Updates

---
+++ precache-manifest.021ba10b5523a7cfca93dd5dc229372a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd598047739f8f08583ed9735a1e989",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "c61ceea2d6a8c780672e",
+    "url": "/hackernews/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/hackernews/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61ceea2d6a8c780672e",
+    "url": "/hackernews/static/js/main.961a4910.chunk.js"
+  },
+  {
+    "revision": "914cab7ae5a96bbcbf0a",
+    "url": "/hackernews/static/js/runtime-main.18e8b03f.js"
+  }
+]);
